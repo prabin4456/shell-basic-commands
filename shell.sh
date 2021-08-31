3c572be79d007a5d3ea701374386862af4bb4f7e
Prabin <76979813+prabin4456@users.noreply.github.com>
2021-08-31 16:06:07 +0545

    Add files via upload

---
+++ shell.sh
@@ -0,0 +1,12 @@
+nano shfile.sh
+#! /bin/bash
+echo "Hello World"
+ctrl X to save
+ls
+bash shfile.sh
+./shfile.sh
+# 2 file 
+cd /storage/emulated/0/
+ls
+bash shfile.sh
+# easy to run code tetmux shell
