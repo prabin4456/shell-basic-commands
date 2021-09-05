e31aca46926d9bf49fdbc0a63a960c1e649d45ff
Prabin <76979813+prabin4456@users.noreply.github.com>
2021-09-05 10:02:19 +0545

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,14 +1,14 @@
-nano shfile.sh
+nano name.sh
 #! /bin/bash
 echo "Hello World"
 ctrl X to save
 ls
-bash shfile.sh
+bash name.sh
 ./shfile.sh
 # 2 file 
 cd /storage/emulated/0/
 ls
-bash shfile.sh
+bash name.sh
 # easy to run code tetmux shell
 chmod +x install.sh
 bash name.sh
